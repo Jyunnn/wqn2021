951d0c5d1668285d6d14e2746c08cd190b462134
Jyunnn <shamamigo@gmail.com>
2021-03-22 00:47:41 +0800

    翻譯一些內容, 停止註冊

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => '密碼已重製',
+    'sent' => '已經通過電子郵件發送了您的密碼重置鏈接',
+    'throttled' => '請稍後重試',
+    'token' => '此密碼重置token無效',
+    'user' => "找不到使用該電子郵件地址的用戶",
 
 ];
